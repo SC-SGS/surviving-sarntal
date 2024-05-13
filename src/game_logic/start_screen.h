1029a342ab275d917d02e45835964b81e5ac8ed8
Daniel Six <st177854@stud.uni-stuttgart.de>
2024-05-13 16:16:05 +0200

    Resolve "Migrate Start Screen"

---
+++ src/game_logic/start_screen.h
@@ -0,0 +1,13 @@
+//
+// Created by six on 5/4/24.
+//
+
+#ifndef SURVIVING_SARNTAL_START_SCREEN_H
+#define SURVIVING_SARNTAL_START_SCREEN_H
+
+class StartScreen {
+  public:
+    void draw();
+};
+
+#endif // SURVIVING_SARNTAL_START_SCREEN_H
